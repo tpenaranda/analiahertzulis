13d5cde1b1685028ee2811628013067321444464
Tate Peñaranda <tate@tpenaranda.com>
2020-02-22 04:50:54 +0100

    Added about and contact.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="max-w-md w-full lg:max-w-full lg:flex p-2 lg:py-16 lg:px-32 m-auto">
+      <div class="flex flex-col justify-center border-l border-t border-r lg:border-r-0 lg:border-b border-gray-400 bg-white p-4">
+        <img class="hidden lg:block" src="../assets/about.jpg">
+        <img class="block lg:hidden" src="../assets/about1.jpg">
+      </div>
+      <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-center leading-normal">
+        <div class="mb-8">
+          <div class="text-gray-900 font-bold text-xl mb-6">Analía Alejandra Hertzulis</div>
+          <div class="text-gray-700 text-base text-justify">
+            <p class="mb-3">Artista, diseñadora, ilustradora y muralista. Nació en Júpiter en el año 1944, hija de padre alfarero y madre ingeniera química Analía desde muy temprana edad comienza a jugar con los pinceles.</p>
+            <p class="mb-3">Continúá jugando durante su adolescencia y a principios de su etapa adulta descubre que los mismos se usan con pintura. Así, a los 28 años, comienza a pintar. Su carrera evoluciona velozmente y 3 años más tarde gana dos premios nobel.</p>
+            <p class="mb-3">A los 289 años se radica en Barcelona, donde tiene 3 hijos y pinta la capilla sixtina, sin dudas su obra más importante, aunque la más contraversial, sustancialmente por no contar con autorización del Vaticano. Disfrazada de migitorio pasa la noche en el baño del recinto y durante la madrugada da color a su gran obra.</p>
+            <p class="mb-3">El suceso divide las aguas del mundo entero. Los medios hablan de atentado, graffiteros del mundo aclaman la obra y corren rumores de complicidad del papa argentino. Francisco declararía años más tarde "me rompían las bolas esos deditos que no se tocaban". Es condenada a la hoguera pero encuentra asilo político en Argentina, donde es recibida con honores por el presidente Marcelo Tinelli.</p>
+            <p class="mb-3">Actualmente reside en Chascomús donde tiene un taller mecánico de tren delantero y frenos, trabajos garantizados y descuentos a jubilados.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
